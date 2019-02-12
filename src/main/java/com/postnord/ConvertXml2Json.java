a4b19e77643413cfc4c2d3d1b7b5569ea7772405
vigneshvr2812 <32358904+vigneshvr2812@users.noreply.github.com>
2019-02-12 19:37:17 +0100

    Added a new method convertJavaObjectToJsonObject
    This method is used to convert Java object to Json String.
    Added Gson dependency to convert Object to Json.

--- src/main/java/com/postnord/ConvertXml2Json.java
+++ src/main/java/com/postnord/ConvertXml2Json.java
@@ -1,19 +1,25 @@
 package com.postnord;
- 
+
+import com.google.gson.Gson;
 import org.json.JSONObject;
 import org.json.XML;
 
 public class ConvertXml2Json {
-	
-	public static String convertJsonObject2Xml(JSONObject json) {
-		String xml = XML.toString(json);				
-		return xml;
-	}
 
-	public static String convertJsonString2Xml(String jsonString) {
-				
-				JSONObject json = new JSONObject(jsonString);
-				String xml = XML.toString(json);				
-				return xml;
-	}
+    public static String convertJsonObject2Xml(JSONObject json) {
+        String xml = XML.toString(json);
+        return xml;
+    }
+
+    public static String convertJsonString2Xml(String jsonString) {
+
+        JSONObject json = new JSONObject(jsonString);
+        String xml = XML.toString(json);
+        return xml;
+    }
+
+    public static String convertJavaObjectToJsonString(Object simpleObject) {
+        Gson gson = new Gson();
+        return gson.toJson(simpleObject);
+    }
 }
